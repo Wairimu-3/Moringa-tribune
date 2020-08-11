8c51c956dd6b298b2ccd170ca5c7a9cf6477b7a3
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2020-08-11 13:49:31 +0300

    apiview

--- news/models.py
+++ news/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 # from tinymce.models import HTMLField
 
 # Create your models here.
+class MoringaMerch(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
@@ -24,12 +34,12 @@ class tags(models.Model):
         return self.name
 
 class Article(models.Model):
-    title = models.CharField(max_length =60)
-    post = models.TextField()
+    title = models.CharField(max_length=60)
+    post = HTMLField()
     editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/')
+    article_image = models.ImageField(upload_to='articles/', blank=True)
     
     @classmethod
     def todays_news(cls):
--- news/views.py
+++ news/views.py
@@ -2,26 +2,66 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+from .forms import NewArticleForm, NewsLetterForm
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  MoringaMerch
+from .serializer import MerchSerializer
+from rest_framework import status
 
 # Create your views here.
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = MoringaMerch.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = MerchSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)   
+
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    # if request.method == 'POST':
+    #     form = NewsLetterForm(request.POST)
+    #     if form.is_valid():
+    #         # print('valid')
+    #         name = form.cleaned_data['your_name']
+    #         email = form.cleaned_data['email']
+    #         recipient = NewsLetterRecipients(name = name,email =email)
+    #         recipient.save()
+    #         send_welcome_email(name,email)
 
-def welcome(request):
-    return render(request, 'welcome.html')
 
-def news_of_day(request):
-    date = dt.date.today()
-    return render(request, 'all-news/today-news.html',{'date': date})
+    #         HttpResponseRedirect('news_today')
+    # else:
+    form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html',{'date': date,"news":news,"letterForm":form})
 
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
 
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
 
-def past_days_news(request, past_date):
+def past_days_news(request,past_date):
+    
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
@@ -29,12 +69,12 @@ def past_days_news(request, past_date):
 
     if date == dt.date.today():
         return redirect(news_today)
-
+    
     news = Article.days_news(date)
-    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
+    return render(request, 'all-news/past-news.html', {"date":date,"news":news})
 
 def search_results(request):
-
+    
     if 'article' in request.GET and request.GET["article"]:
         search_term = request.GET.get("article")
         searched_articles = Article.search_by_title(search_term)
@@ -44,11 +84,28 @@ def search_results(request):
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-news/search.html',{"message":message} ) 
+        return render(request, 'all-news/search.html',{"message":message})
 
+@login_required(login_url='/accounts/login/')    
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"all-news/article.html", {"article":article})           
+    return render(request,"all-news/article.html", {"article":article})
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
+
+        
